36df26c4ca800d2d18ec9ea26f4037794b58b5be
Shayaan Siddiqui <shayaan.siddiqui@mac.com>
2019-01-31 10:12:53 -0500

    Fixed Interface Builder

--- App-Swoosh/App-Swoosh/Controller/WelcomeVC.swift
+++ App-Swoosh/App-Swoosh/Controller/WelcomeVC.swift
@@ -9,8 +9,8 @@
 import UIKit
 
 class WelcomeVC: UIViewController {
-    //@IBOutlet weak var swoosh: UIImageView!
-    //@IBOutlet weak var bgImg: UIImageView!
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()        
